a2439fcef0d0a1e88a011d310520860be33b3a06
venkateswarasticker <venkateswarastickerdecor@gmail.com>
2025-03-30 11:29:34 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://67e804c3a597770315377340--srivenkateswarastickers.netlify.app/");
+    }
+}
